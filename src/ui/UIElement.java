b5ac93c6b02245508637703a1ce8aeda36b60622
SwingsOberon <Swings.Oberon@hotmail.com>
2020-03-04 16:48:14 +0100

    getters setters

---
+++ src/ui/UIElement.java
@@ -0,0 +1,25 @@
+package ui;
+
+
+class UIElement{
+    private int xPos;
+    private int yPos;
+
+	public int getXPos() {
+		return this.xPos;
+	}
+
+	public void setXPos(int xPos) {
+		this.xPos = xPos;
+	}
+
+	public int getYPos() {
+		return this.yPos;
+	}
+
+	public void setYPos(int yPos) {
+		this.yPos = yPos;
+	}
+
+    
+}
---
+++ src/ui/UIRobot.java
@@ -0,0 +1,14 @@
+package ui;
+
+class UIRobot {
+    private int direction;
+
+    public int getDirection() {
+        return this.direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+}

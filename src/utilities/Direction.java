c2ad762ee30edd286ee44c4b6a96073fe5754d02
Bert De Vleeschouwer <bert.d.vl.schouwer@gmail.com>
2020-03-07 16:20:04 +0100

    added a direction utility class

---
+++ src/utilities/Direction.java
@@ -0,0 +1,29 @@
+package utilities;
+
+public final class Direction{
+
+    public static final int UP = 0;
+
+    public static final int RIGHT = 1;
+  
+    public static final int DOWN = 2;
+    
+    public static final int LEFT = 3;
+    
+    private int direction;
+
+    public Direction(int direction){
+        if(direction >-1 && direction < 3){
+            this.direction = direction;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid direction given");
+        }
+    }
+    
+    public int getDirection() {
+        return this.direction;
+    }
+
+    
+}

f764a651a40ba44ba0ec541183626403229028bf
Jesse Geens <jesse.geens@gmail.com>
2020-03-09 16:57:45 +0100

    setup test environment

---
+++ test/model/ModelControllerTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ModelControllerTest {
+
+    @Test
+    public void testMoveBlock(){}
+
+    @Test
+    public void testFindClosestBlock(){}
+
+    @Test
+    public void testFindLeftNeighbour(){}
+}

fa19ecdb475928b258c9cf95fbb3834560cbc24c
Jesse Geens <jesse.geens@gmail.com>
2020-03-12 11:32:18 +0100

    UIBlock now takes a modelblock as argument

--- src/ui/blocks/UIBlock.java
+++ src/ui/blocks/UIBlock.java
@@ -1,22 +1,24 @@
 package ui.blocks;
 import ui.*;
 import java.awt.*;
+
+import model.blocks.ModelBlock;
 import utilities.*;
 
 public class UIBlock extends UIElement {
 
-    final int width = 120;
-    final int height = 120;
-
-    final Blocktype blockType;
+    int width;
+    int height;
+    ModelBlock mBlock;
 
 
 
     // Constructor
-    public UIBlock(Location location, Blocktype blockType) {
+    public UIBlock(Location location, ModelBlock mBlock) {
         super(location);
-        this.blockType = blockType;
-        
+        this.width = mBlock.getWidth();
+        this.height = mBlock.getHeight();    
+        this.mBlock = mBlock;
     }
 
     /**
@@ -25,7 +27,7 @@ public class UIBlock extends UIElement {
      * @param {Graphics} g The Graphics object on which the block is rendered
      */
     public void render(Graphics g){
-        switch (blockType.getType()){
+        switch (mBlock.getBlockType().getType()){
             case Blocktype.IF:
                 g.setColor(Color.GRAY);
                 g.fillRect(this.getPos().getX(),this.getPos().getY(), (this.width / 3), this.height);
@@ -33,7 +35,7 @@ public class UIBlock extends UIElement {
                 g.fillRect(this.getPos().getX() + (2*width/3),this.getPos().getY(),(width/3), (height/2));
                 g.fillRect(this.getPos().getX() + (2*width/3),this.getPos().getY() + (3*height/4),(width/3), (height/4));
                 g.setColor(Color.WHITE);
-                g.drawString(blockType.getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
+                g.drawString(mBlock.getBlockType().getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
                 break;
             case Blocktype.WHILE:
                 g.setColor(Color.GRAY);
@@ -42,7 +44,7 @@ public class UIBlock extends UIElement {
                 g.fillRect(this.getPos().getX() + (2*width/3),this.getPos().getY(),(width/3), (height/2));
                 g.fillRect(this.getPos().getX() + (2*width/3),this.getPos().getY() + (3*height/4),(width/3), (height/4));
                 g.setColor(Color.WHITE);
-                g.drawString(blockType.getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
+                g.drawString(mBlock.getBlockType().getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
                 break;
             case Blocktype.MOVEFORWARD:
                 g.setColor(Color.GRAY);
@@ -50,7 +52,7 @@ public class UIBlock extends UIElement {
                 g.fillRect(this.getPos().getX() + (width/3),this.getPos().getY()+(height/4), (width/3), height);
                 g.fillRect(this.getPos().getX() + (2*width/3),this.getPos().getY(),(width/3), height);
                 g.setColor(Color.WHITE);
-                g.drawString(blockType.getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2)); 
+                g.drawString(mBlock.getBlockType().getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2)); 
                 break;
             case Blocktype.TURNLEFT:
                 g.setColor(Color.GRAY);
@@ -58,7 +60,7 @@ public class UIBlock extends UIElement {
                 g.fillRect(this.getPos().getX() + (width/3),this.getPos().getY()+(height/4), (width/3), height);
                 g.fillRect(this.getPos().getX() + (2*width/3),this.getPos().getY(),(width/3), height);
                 g.setColor(Color.WHITE);
-                g.drawString(blockType.getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
+                g.drawString(mBlock.getBlockType().getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
                 break;
             case Blocktype.TURNRIGHT:
                 g.setColor(Color.GRAY);
@@ -66,14 +68,14 @@ public class UIBlock extends UIElement {
                 g.fillRect(this.getPos().getX() + (width/3),this.getPos().getY()+(height/4), (width/3), height);
                 g.fillRect(this.getPos().getX() + (2*width/3),this.getPos().getY(),(width/3), height);
                 g.setColor(Color.WHITE);
-                g.drawString(blockType.getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
+                g.drawString(mBlock.getBlockType().getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
                 break;
             case Blocktype.WALLINFRONT:
                 g.setColor(Color.GRAY);
                 g.fillRect(this.getPos().getX() + (width / 4),this.getPos().getY(), width, height);
                 g.fillRect(this.getPos().getX(),this.getPos().getY() + (height / 3), (width/4), height/3);
                 g.setColor(Color.WHITE);
-                g.drawString(blockType.getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
+                g.drawString(mBlock.getBlockType().getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
                 break;
             case Blocktype.NOT:
                 g.setColor(Color.GRAY);
@@ -81,7 +83,7 @@ public class UIBlock extends UIElement {
                 g.fillRect(this.getPos().getX() - (width/4),this.getPos().getY()+(height/3), height, (width/3));
                 g.fillRect(this.getPos().getX(),this.getPos().getY() +(2*height/3),width, (height/3));
                 g.setColor(Color.WHITE);
-                g.drawString(blockType.getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
+                g.drawString(mBlock.getBlockType().getTitle(), this.getPos().getX() + 10, this.getPos().getY() + (height/2));
                 break;
             default:
                 throw new IllegalArgumentException("Invalid block type in case statement");

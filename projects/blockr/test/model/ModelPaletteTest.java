1a1c89561532991546bfbc444bf9f1777c0d9357
SwingsOberon <Swings.Oberon@hotmail.com>
2020-04-08 12:42:44 +0200

    palette tests completed, now starting on ProgramArea tests

--- projects/blockr/test/model/ModelPaletteTest.java
+++ projects/blockr/test/model/ModelPaletteTest.java
@@ -8,48 +8,30 @@ import org.junit.Test;
 import utilities.*;
 
 public class ModelPaletteTest {
-/*
-    @Test
-    public void blockToProgramWindow() {
-        ModelPalette palette = new ModelPalette(1000,1000);
-        palette.setTurnLeftBlock(null);
-        ModelBlock blk = new ModelMoveBlock(new Location(100,100), BlockType.TURNLEFT);
-        palette.blockToProgramArea(blk,false);
-        assertEquals(7, palette.getPaletteBlocks().size());
-    }
-
-    @Test
-    public void blockToProgramWindowLimit() {
-        ModelPalette palette = new ModelPalette(1000,1000);
-        palette.setTurnLeftBlock(null);
-        ModelBlock blk = new ModelMoveBlock(new Location(100,100), BlockType.TURNLEFT);
-        palette.blockToProgramArea(blk,true);
-        assertEquals(0, palette.getPaletteBlocks().size());
-    }
 
     @Test
     public void resetBlocks() {
         ModelPalette palette = new ModelPalette(1000,1000);
-        palette.setTurnLeftBlock(null);
         palette.resetBlocks();
-        assertFalse(palette.getTurnLeftBlock() == null);
+        assertEquals(7, palette.getPaletteBlocks().size());
     }
 
     @Test
-    public void handleMouseDownLeft() {
+    public void removeBlocks(){
         ModelPalette palette = new ModelPalette(1000,1000);
-        assertEquals(palette.getTurnLeftBlock(), palette.handleMouseDown(new Location(60,60), false));
+        palette.removeBlocks();
+        assertTrue(palette.getPaletteBlocks().isEmpty());
     }
 
     @Test
     public void handleMouseDownIf() {
         ModelPalette palette = new ModelPalette(1000,1000);
-        assertEquals(palette.getIfBlock(), palette.handleMouseDown(new Location(60,540), false));
+        assertEquals(BlockType.IF, palette.handleMouseDown(new Location(60,540)).getBlockType());
     }
 
     @Test
     public void getPaletteBlocks() {
         ModelPalette palette = new ModelPalette(1000,1000);
         assertEquals(7, palette.getPaletteBlocks().size());
-    }*/
+    }
 }

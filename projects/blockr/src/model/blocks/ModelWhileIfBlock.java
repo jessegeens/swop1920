7d68732649060b7f67fd88eaee386e03433cdb23
SwingsOberon <Swings.Oberon@hotmail.com>
2020-05-09 11:25:44 +0200

    Added superclass for WhileIfBlock and FunctionDefinitionBlock named ModelCavityBlock

--- projects/blockr/src/model/blocks/ModelWhileIfBlock.java
+++ projects/blockr/src/model/blocks/ModelWhileIfBlock.java
@@ -10,12 +10,10 @@ import utilities.*;
  * Class representing the while and if blocks with one socket on the top and one plug at the bottom. They also have one socket on their 
  * right side for a condition block.
  */
-public class ModelWhileIfBlock extends ModelBlock{
+public class ModelWhileIfBlock extends ModelCavityBlock{
     private ModelBlock topSocket;
     private ModelBlock bottomPlug;
     private ModelBlock rightSocket;
-    private ModelBlock cavitySocket;
-    private ModelBlock cavityPlug;
     private boolean isIf;
 
     // Constructor
@@ -25,21 +23,16 @@ public class ModelWhileIfBlock extends ModelBlock{
         this.setTopSocket(null);
         this.setBottomPlug(null);
         this.setRightSocket(null);
-        this.setCavityPlug(this);
-        this.setCavitySocket(this);
-        ArrayList<ConnectionPoint> connectionPoints = new ArrayList<>();
+        ArrayList<ConnectionPoint> connectionPoints = super.getConnectionPoints();
         connectionPoints.add(ConnectionPoint.BOTTOM_PLUG);
         connectionPoints.add(ConnectionPoint.TOP_SOCKET);
-        connectionPoints.add(ConnectionPoint.CAVITY_PLUG);
-        connectionPoints.add(ConnectionPoint.CAVITY_SOCKET);
         connectionPoints.add(ConnectionPoint.RIGHT_SOCKET);
         super.setConnectionPoints(connectionPoints);
     }
 
     @Override
     public ModelWhileIfBlock clone() {
-        if (this.isIf) { return new ModelWhileIfBlock(this.getPos(), true); }
-        return new ModelWhileIfBlock(this.getPos(), false);
+        return new ModelWhileIfBlock(this.getPos(), isIf);
     }
 
     /**
@@ -111,39 +104,6 @@ public class ModelWhileIfBlock extends ModelBlock{
     }
 
     /**
-     * Getter for the height of the cavity of the while and if block.
-     * @return the height of the block
-     * @author Oberon Swings
-     */
-    public int getCavityHeight() {
-        if(!(getCavityBlocks().size() == 0)){
-            return getCavityBlocks().size() * UIBlock.STD_HEIGHT;
-        }
-        else{
-            return 0;
-        } 
-    }
-
-    /**
-     * Getter for a list of blocks this block has in its cavity.
-     * @return the list of blocks.
-     * @author Oberon Swings
-     */
-    public ArrayList<ModelBlock> getCavityBlocks() {
-        ArrayList<ModelBlock> cav = new ArrayList<ModelBlock>();
-        ModelBlock blk = this.getCavityPlug();
-        while(!(blk.equals(this) || blk == null)){
-            cav.add(blk);
-            if (blk instanceof ModelWhileIfBlock) cav.addAll(((ModelWhileIfBlock) blk).getCavityBlocks());
-            if (blk == null) return null;
-            if(blk.hasBottomPlug()){
-                blk = blk.getBottomPlug();
-            }
-        }
-        return cav;
-    }
-
-    /**
      * {@inheritDoc}
      */
     public ModelBlock getRightSocket() {
@@ -155,73 +115,5 @@ public class ModelWhileIfBlock extends ModelBlock{
      */
     public void setRightSocket(ModelBlock rightSocket) {
         this.rightSocket = rightSocket;
-    }
-
-    /**
-     * 
-     * @return the cavity of the socket.
-     */
-    public ModelBlock getCavitySocket() {
-        return this.cavitySocket;
-    }
-
-    /**
-     * 
-     * @param cavitySocket sets the cavity of the socket.
-     */
-    public void setCavitySocket(ModelBlock cavitySocket) {
-        this.cavitySocket = cavitySocket;
-    }
-
-    /**
-     * 
-     * @return the cavity of the plug.
-     */
-    public ModelBlock getCavityPlug() {
-        return this.cavityPlug;
-    }
-
-    /**
-     * 
-     * @param cavityPlug sets the cavity of the plug.
-     */
-    public void setCavityPlug(ModelBlock cavityPlug) {
-        this.cavityPlug = cavityPlug;
-    }
-
-    /**
-     * 
-     * @return the position of the cavity socket
-     * @author Oberon Swings
-     */
-    public ProgramLocation getCavitySocketPos() {
-        return this.getPos().add(2*UIBlock.STD_WIDTH/3, this.getHeight() - UIBlock.STD_HEIGHT/3);
-    }
-
-    /**
-     * 
-     * @return the position of the cavity plug
-     * @author Oberon Swings
-     */
-    public ProgramLocation getCavityPlugPos() {
-        return this.getPos().add(2*UIBlock.STD_WIDTH/3, 2*UIBlock.STD_HEIGHT/3);
-    }
-
-    /**
-     * Calculates the distance between cavityPlug and TopSocket
-     * @param top the block with the topSocket
-     * @return the distance form the cavityPlug to the topSocket of the top block
-     */
-    public int distanceCavityPlug(ModelBlock top){
-        return this.getCavityPlugPos().getDistance(top.getTopSocketPos());
-    }
-
-    /**
-     * Calculates the distance between cavitySocket and bottomPlug
-     * @param bottom the block with the bottomPlug
-     * @return the distance form the cavitySocket to the bottomPlug of the bottom block
-     */
-    public int distanceCavitySocket(ModelBlock bottom){
-        return this.getCavitySocketPos().getDistance(bottom.getBottomPlugPos());
     }
 }
